1f940ae5e48feee45c74e9c5591eb35bb32dc2b3
Arpit Mourya <112952232+Arpitmourya1104@users.noreply.github.com>
2024-11-16 03:45:24 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,14 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import ClientViewSet, ProjectViewSet
 
-router = DefaultRouter()
-router.register(r'clients', ClientViewSet)
-router.register(r'projects', ProjectViewSet)
+from django.contrib import admin
+from django.urls import path,include
+from api.views import CompanyViewSEt, EmployeeViewSEt
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'companies',CompanyViewSEt)
+router.register(r'employees', EmployeeViewSEt)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(router.urls))
+ 
 ]
